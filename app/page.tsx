9b8210172e885eb7c2b6e1f6a49c69261f340405
Goran SD <g.cvetic23@gmail.com>
2024-08-06 22:19:17 -0700

    chore: Update homepage with button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
