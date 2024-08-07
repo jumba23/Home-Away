4d0173edf684d819dd6b0ab01f685b6ec9122eb7
Goran SD <g.cvetic23@gmail.com>
2024-08-06 23:43:41 -0700

    feat: Update Logo and NavSearch components in the Navbar

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -3,7 +3,13 @@ import { LuTent } from "react-icons/lu";
 import { Button } from "../ui/button";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <LuTent className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,13 @@
+import { Input } from "../ui/input";
+
 const NavSearch = () => {
-  return <div>NavSearch</div>;
+  return (
+    <Input
+      placeholder="find a property..."
+      type="text"
+      className="max-w-xs dark:bg-muted"
+    />
+  );
 };
 
 export default NavSearch;
