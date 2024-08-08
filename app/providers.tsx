37b218400f83d4fe59623174aaaeee693cf221c7
Goran SD <g.cvetic23@gmail.com>
2024-08-07 22:29:10 -0700

    refactor: Update ThemeProvider to use NextThemesProvider from next-themes package

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { ThemeProvider } from "./theme-provider";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Providers;
