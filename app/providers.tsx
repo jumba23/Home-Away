08f32e5a7485c9d61c2cb46ae3e8f3df778381ec
Goran SD <g.cvetic23@gmail.com>
2024-08-08 09:27:19 -0700

    feat: Add Toaster component for displaying notifications

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,21 @@
+"use client"; // since Toast is a hook  and it uses useAuth
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+
 const SignOutLink = () => {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  const handleSignOut = () => {
+    toast({ description: "You have signed out." });
+  };
+
+  return (
+    <SignOutButton>
+      <button className="w-full text-left" onClick={handleSignOut}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 };
 
 export default SignOutLink;
