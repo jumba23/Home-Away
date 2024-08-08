ce5e50c75be42d0012bd78825e6cbe7a80e93932
Goran SD <g.cvetic23@gmail.com>
2024-08-07 23:07:19 -0700

    feat: Update metadata in RootLayout for HomeAway Draft

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,11 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HomeAway",
-  description: "Feel At Home, Away From Home",
+  title: "HomeAway Draft",
+  description: "Feel at home, away from home.",
 };
 
 export default function RootLayout({
