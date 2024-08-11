0cd4c45a7616db316f6688bbcc36aecc2622bb35
Goran SD <g.cvetic23@gmail.com>
2024-08-10 23:45:22 -0700

    feat: Update CreateProfilePage component with form fields and styling

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const createProfileAction = async (formData: FormData) => {
+  "use server";
+  const firstName = formData.get("firstName") as string;
+  console.log(firstName);
+};
 
 const CreateProfilePage = () => {
-  return <div>CreateProfilePage</div>;
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md max-w-lg">
+        <form action={createProfileAction}>
+          <div className="mb-2">
+            <Label htmlFor="firstName">First Name</Label>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default CreateProfilePage;
