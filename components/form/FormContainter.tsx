b766aa14ed50286255d4d67df5629ec2ee5c1585
Goran SD <g.cvetic23@gmail.com>
2024-08-11 09:01:45 -0700

    feat: Refactor FormContainer component to use useFormState hook and add form action prop

--- components/form/FormContainter.tsx
+++ components/form/FormContainter.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import { useFormState } from "react-dom";
-import { useEffect } from "react";
+import useEffect from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { actionFunction } from "@/utils/types";
 
 const initialState = {
   message: "",
+};
+
+export const FormContainer = ({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) => {
+  const [state, formAction] = useFormState(action, initialState);
+
+  return <form action={}>{children}</form>;
 };
