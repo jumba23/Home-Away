75ca1c4005ba8810dd151c8bafd59f5c9ca93bd3
Goran SD <g.cvetic23@gmail.com>
2024-08-11 08:55:55 -0700

    Refactor CreateProfilePage to use FormInput component and remove unused imports

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 const createProfileAction = async (formData: FormData) => {
   "use server";
   const firstName = formData.get("firstName") as string;
@@ -11,11 +9,7 @@ const CreateProfilePage = () => {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
-        </form>
+        <form action={createProfileAction}></form>
       </div>
     </section>
   );
---
+++ components/form/FormContainter.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { actionFunction } from "@/utils/types";
+
+const initialState = {
+  message: "",
+};
