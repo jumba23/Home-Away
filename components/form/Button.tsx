a031e9ff7df8abab72b455b03b63ec426e301302
Goran SD <g.cvetic23@gmail.com>
2024-08-11 00:11:38 -0700

    feat: Refactor SubmitButton component to use useFormStatus hook for disabling button during form submission

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -8,3 +8,27 @@ type SubmitButtonProps = {
   className?: string;
   text?: string;
 };
+
+export const SubmitButton = ({
+  className = "",
+  text = "submit",
+}: SubmitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      size="lg"
+      className={`capitalize ${className}`}
+      disabled={pending}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 w-4 h-4 animate-spin" /> Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+};
