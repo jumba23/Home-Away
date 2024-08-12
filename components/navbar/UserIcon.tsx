be45c03ec87a68817a3793e371f15c5637b6ca7f
Goran SD <g.cvetic23@gmail.com>
2024-08-12 15:11:06 -0700

    feat: Add lazy loading for user profile image in UserIcon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,18 @@
 import { LuUser2 } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
+const UserIcon = async () => {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt="profile"
+        className="w-6 h-6 bg-primary rounded-full text-white"
+      />
+    );
+  }
 
-const UserIcon = () => {
-  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+  // return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 };
 
 export default UserIcon;
