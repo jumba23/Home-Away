0d18dc2ccb359a74177c40015c9aa9307d6e838d
Goran SD <g.cvetic23@gmail.com>
2024-08-12 14:43:39 -0700

    feat: Add Profile model to schema.prisma

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,31 @@
 "use server";
 
 import { profileSchema } from "./schemas";
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
+    await db.profile.create({
+      data: {
+        // First three values do not exist in our object
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+
     return { message: "profile created" };
   } catch (error) {
     console.log(error);
