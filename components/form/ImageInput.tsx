e576c911bdd166c05f1c49be6a130f84dc72740b
Goran SD <g.cvetic23@gmail.com>
2024-08-12 23:27:20 -0700

    feat: Add ImageInput component
    This commit adds the ImageInput component to the components/form directory. The component provides an input field for uploading images and is used in various forms throughout the application. This change enhances the user experience by allowing users to easily upload images when required.
    
    Co-authored-by: [Author Name]

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+const ImageInput = () => {
+  const name = "image";
+
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        Image
+      </Label>
+      <Input
+        type="file"
+        name={name}
+        id={name}
+        required
+        accept="image/*"
+        className="max-w-xs"
+      />
+    </div>
+  );
+};
+
+export default ImageInput;
