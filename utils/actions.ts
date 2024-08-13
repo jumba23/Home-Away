97b407c26824d19866b1296a88a0b9d6991caada
Goran SD <g.cvetic23@gmail.com>
2024-08-12 22:13:11 -0700

    feat: Add fetchProfile function
    This commit adds the fetchProfile function to the utils/actions.ts file. The function retrieves the user's profile from the database using Prisma. It first checks if the user is logged in, and if so, queries the database for the profile associated with the user's clerkId. If no profile is found, the page is redirected to the create profile page. This change enables the application to fetch and display the user's profile when needed.

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,15 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this route");
+  if (!user.privateMetadata.hasProfile) {
+    redirect("/profile/create");
+  }
+  return user;
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -56,4 +65,14 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
 };
