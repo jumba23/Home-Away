8b733ed3391a0dbdc342463d4c8c3874c9de64e8
Goran SD <g.cvetic23@gmail.com>
2024-08-13 10:38:37 -0700

    refactor: Update uploadImage function to handle failed image uploads

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -10,7 +10,9 @@ const supabase = createClient(url, key);
 const uploadImage = async (image: File) => {
   const timestamp = Date.now();
   const newName = `${timestamp}-${image.name}`;
-  const { data, error } = await supabase.storage
+  const { data } = await supabase.storage
     .from(bucket)
     .upload(newName, image, { cacheControl: "3600" });
+  if (!data) throw new Error("Image upload failed");
+  return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
