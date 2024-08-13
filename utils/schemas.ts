1b761b0bd0c6b8ff67e9fac50f0f5864439f5226
Goran SD <g.cvetic23@gmail.com>
2024-08-12 23:06:26 -0700

    feat: Update profileSchema validation messages
    This commit updates the validation messages in the profileSchema of the utils/schemas.ts file. The validation messages for the firstName, lastName, and username fields have been modified to provide more specific instructions to the user. This change improves the clarity of error messages and enhances the user experience when filling out the profile creation form.
    
    Co-authored-by: [Author Name]

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -3,7 +3,13 @@ import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
   // firstName: z.string().max(5, {message: 'max length is 5'}),
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  username: z.string().min(2),
+  firstName: z
+    .string()
+    .min(2, { message: "firs name must be at least 2 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "last name must be at least 2 characters" }),
+  username: z
+    .string()
+    .min(2, { message: "username must be at least 2 characters" }),
 });
