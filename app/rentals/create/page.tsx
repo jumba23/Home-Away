d4abab138c985e58314d98510e990d976d285a3f
Goran SD <g.cvetic23@gmail.com>
2024-08-13 12:16:02 -0700

    feat: Add CreatePropertyPage component for property creation

---
+++ app/rentals/create/page.tsx
@@ -0,0 +1,38 @@
+import FormInput from "@/components/form/FormInput";
+import { FormContainer } from "@/components/form/FormContainer";
+import { createPropertyAction } from "@/utils/actions";
+import { SubmitButton } from "@/components/form/Buttons";
+
+const CreatePropertyPage = () => {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">
+        Create Property
+      </h1>
+      <div className="border p-8 rounded">
+        <h3 className="text-lg mb-4 font-medium">General Info</h3>
+        <FormContainer action={createPropertyAction}>
+          <div className="grid md:grid-cols-2 gap-8 mb-4">
+            <FormInput
+              name="name"
+              type="text"
+              label="Name (20 limit)"
+              defaultValue="Cabin in Latvia"
+            />
+            <FormInput
+              name="tagline"
+              type="text"
+              label="Tagline (30 limit)"
+              defaultValue="Dream Getaway Awaits You Here"
+            />
+            {/* price */}
+            {/* categories */}
+          </div>
+          <SubmitButton text="Create Rental" className="mt-12" />
+        </FormContainer>
+      </div>
+    </section>
+  );
+};
+
+export default CreatePropertyPage;
--- app/rentals/create/pages.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreatePropertyPage = () => {
-  return <div>CreatePropertyPage</div>;
-};
-
-export default CreatePropertyPage;
