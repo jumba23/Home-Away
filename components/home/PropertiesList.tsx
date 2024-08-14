e705ab26058c66994e9f3d683caa57bcd9c194cc
Goran SD <g.cvetic23@gmail.com>
2024-08-14 14:18:08 -0700

    feat: Update PropertiesList component to use grid layout with responsive columns
    This commit modifies the PropertiesList component in the components/home/PropertiesList.tsx file. The component now uses a grid layout with responsive columns to display the list of properties. This change improves the visual presentation of the properties on the home page.
    
    Co-authored-by: [Author Name]

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -7,7 +7,7 @@ const PropertiesList = ({
   properties: PropertyCardProps[];
 }) => {
   return (
-    <section>
+    <section className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {properties.map((property) => (
         <PropertyCard key={property.id} property={property} />
       ))}
