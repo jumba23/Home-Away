7b31572368a37923aaf83a0f6b1c9fbe2f2afdad
Goran SD <g.cvetic23@gmail.com>
2024-08-14 11:06:58 -0700

    feat: Add hidden input for count in CounterInput component

--- components/form/CounterInput.tsx
+++ components/form/CounterInput.tsx
@@ -30,6 +30,7 @@ const CounterInput = ({
   return (
     <Card className="mb-4">
       {/*input*/}
+      <input type="hidden" name={detail} value={count} />
       <CardHeader className="flex flex-col gap-y-5">
         <div className="flex items-center justify-between flex-wrap">
           <div className="flex flex-col">
