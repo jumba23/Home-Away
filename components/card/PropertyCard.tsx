b4e7ea488dff772e6eda3583024ba76d3c289fe7
Goran SD <g.cvetic23@gmail.com>
2024-08-14 14:02:55 -0700

    feat: Implement lazy loading for property images in PropertyCard component

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CountryFlagAndName from "./CountryFlagAndName";
+import PropertyRating from "./PropertyRating";
+import FavoriteToggleButton from "./FavoriteToggleButton";
+import { PropertyCardProps } from "@/utils/types";
+import { formatCurrency } from "@/utils/format";
+const PropertyCard = ({ property }: { property: PropertyCardProps }) => {
+  const { name, image, price } = property;
+  const { country, id: propertyId, tagline } = property;
 
-const PropertyCard = () => {
-  return <div>PropertyCard</div>;
+  return (
+    <article className="group relative">
+      <Link href={`/properties/${propertyId}`}>
+        <div className="'relative h-[300px] mb-2 overflow-hidden rounded-md">
+          <Image
+            src={image}
+            alt={name}
+            fill
+            objectFit="cover"
+            sizes="(max-width:768) 100vw, 50vw"
+            className="rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500"
+          />
+        </div>
+      </Link>
+    </article>
+  );
 };
 
 export default PropertyCard;
