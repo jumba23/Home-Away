a2e21cd825f3f68a63eb3b92b7bdc55d354cb8e0
Goran SD <g.cvetic23@gmail.com>
2024-08-14 14:15:12 -0700

    feat: Add lazy loading for property images in PropertyCard component

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -5,20 +5,20 @@ import PropertyRating from "./PropertyRating";
 import FavoriteToggleButton from "./FavoriteToggleButton";
 import { PropertyCardProps } from "@/utils/types";
 import { formatCurrency } from "@/utils/format";
-const PropertyCard = ({ property }: { property: PropertyCardProps }) => {
+
+function PropertyCard({ property }: { property: PropertyCardProps }) {
   const { name, image, price } = property;
   const { country, id: propertyId, tagline } = property;
 
   return (
     <article className="group relative">
       <Link href={`/properties/${propertyId}`}>
-        <div className="'relative h-[300px] mb-2 overflow-hidden rounded-md">
+        <div className="relative h-[300px] mb-2 overflow-hidden rounded-md">
           <Image
             src={image}
-            alt={name}
             fill
-            objectFit="cover"
-            sizes="(max-width:768) 100vw, 50vw"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+            alt={name}
             className="rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500"
           />
         </div>
@@ -26,10 +26,23 @@ const PropertyCard = ({ property }: { property: PropertyCardProps }) => {
           <h3 className="text-sm font-semibold mt-1">
             {name.substring(0, 30)}
           </h3>
+          {/* property rating */}
+        </div>
+        <p className="text-sm mt-1 text-muted-foreground ">
+          {tagline.substring(0, 40)}
+        </p>
+        <div className="flex justify-between items-center mt-1">
+          <p className="text-sm mt-1 ">
+            <span className="font-semibold">{formatCurrency(price)} </span>
+            night
+          </p>
+          {/* country and flag */}
         </div>
       </Link>
+      <div className="absolute top-5 right-5 z-5">
+        {/* favorite toggle button */}
+      </div>
     </article>
   );
-};
-
+}
 export default PropertyCard;
