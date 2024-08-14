d675d894f87c5108070f7c05bd88e544eab6a631
Goran SD <g.cvetic23@gmail.com>
2024-08-14 10:46:25 -0700

    feat: Add CounterInput component for counting
    The CounterInput component has been added to the components/form/CounterInput.tsx file. This component allows for counting and will enhance the functionality of the form by providing a dedicated component for counting.

---
+++ components/form/CounterInput.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Card, CardHeader } from "@/components/ui/card";
+import { LuMinus, LuPlus } from "react-icons/lu";
+
+import { Button } from "../ui/button";
+import { useState } from "react";
+const CounterInput = () => {
+  return <div>CounterInput</div>;
+};
+
+export default CounterInput;
