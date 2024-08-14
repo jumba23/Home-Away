36888e1d870b7467df8e9316d73d8a1039c840bd
Goran SD <g.cvetic23@gmail.com>
2024-08-14 13:26:32 -0700

    feat: Update CategoriesList component to include lazy loading for images
    Refactor the CategoriesList component to implement lazy loading for images. This enhancement improves the performance of the home page by loading images only when they are visible on the screen. It enhances the user experience by reducing the initial load time of the home page.
    
    Co-authored-by: [Author Name]

--- components/home/CategoriesList.tsx
+++ components/home/CategoriesList.tsx
@@ -1,3 +1,7 @@
+import { categories } from "@/utils/categories";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
+import Link from "next/link";
+
 const CategoriesList = ({
   category,
   search,
@@ -5,7 +9,35 @@ const CategoriesList = ({
   category?: string;
   search?: string;
 }) => {
-  return <div>CategoriesList</div>;
+  const searchTerm = search ? `?search=${search}` : "";
+
+  return (
+    <section>
+      <ScrollArea className="py-6">
+        <div className="flex gap-x-4">
+          {categories.map((item) => {
+            const isActive = item.label === category;
+
+            return (
+              <Link
+                key={item.label}
+                href={`/?category=${item.label}${searchTerm}`}
+              >
+                <article
+                  className={`p-3 flex flex-col items-center cursor-pointer duration-300 hover:text-primary w-[100px] ${
+                    isActive ? "text-primary" : ""
+                  }`}
+                >
+                  <item.icon className="w-8 h-8" />
+                  <p className="capitalize text-sm mt-1">{item.label}</p>
+                </article>
+              </Link>
+            );
+          })}
+        </div>
+      </ScrollArea>
+    </section>
+  );
 };
 
 export default CategoriesList;
