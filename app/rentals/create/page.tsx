2c323759ab4574b50ee5186b1439a325ca3600d9
Goran SD <g.cvetic23@gmail.com>
2024-08-14 11:02:01 -0700

    feat: Add CounterInput component for counting

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,7 @@ import CategoriesInput from "@/components/form/CategoriesInput";
 import TextAreaInput from "@/components/form/TextAreaInput";
 import CountriesInput from "@/components/form/CountriesInput";
 import ImageInput from "@/components/form/ImageInput";
+import CounterInput from "@/components/form/CounterInput";
 
 const CreatePropertyPage = () => {
   return (
@@ -44,6 +45,13 @@ const CreatePropertyPage = () => {
             <CountriesInput />
             <ImageInput />
           </div>
+          <h3 className="text-lg mt-8 mb-4 font-medium">
+            Accommodation Details
+          </h3>
+          <CounterInput detail="guests" />
+          <CounterInput detail="bedrooms" />
+          <CounterInput detail="beds" />
+          <CounterInput detail="baths" />
           <SubmitButton text="Create Rental" className="mt-12" />
         </FormContainer>
       </div>
