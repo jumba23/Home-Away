5a0e7465ce856dc2cddf93a1a281b1b50e5f468b
Goran SD <g.cvetic23@gmail.com>
2024-08-15 14:24:18 -0700

    feat: Add async/await to FavoriteToggleButton component
    This commit modifies the FavoriteToggleButton component in the components/card/FavoriteToggleButton.tsx file to use async/await syntax. The component now awaits the fetchFavoriteId function to retrieve the favorite ID before rendering the FavoriteToggleForm. This change ensures that the favorite ID is available before rendering the form, improving the functionality of the favorite toggle button.
    
    Co-authored-by: [Author Name]

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -3,8 +3,10 @@ import { Button } from "../ui/button";
 import { auth } from "@clerk/nextjs/server";
 import { Card } from "../ui/card";
 import { CardSignInButton } from "../form/Buttons";
+import { fetchFavoriteId } from "@/utils/actions";
+import FavoriteToggleForm from "./FavoriteToggleForm";
 
-const FavoriteToggleButton = ({ propertyId }: { propertyId: string }) => {
+const FavoriteToggleButton = async ({ propertyId }: { propertyId: string }) => {
   const { userId } = auth();
 
   //if there is no user, return Sign In Modal
@@ -12,13 +14,9 @@ const FavoriteToggleButton = ({ propertyId }: { propertyId: string }) => {
     return <CardSignInButton />;
   }
 
-  const favoriteId = 
+  const favoriteId = await fetchFavoriteId({ propertyId });
 
-  return (
-    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
-      <FaHeart />
-    </Button>
-  );
+  return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />;
 };
 
 export default FavoriteToggleButton;
