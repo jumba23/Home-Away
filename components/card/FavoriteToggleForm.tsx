775e1b4ba2202b7b9a7c57f9ce6e0a15e816c3c9
Goran SD <g.cvetic23@gmail.com>
2024-08-15 14:37:54 -0700

    refactor: Update FavoriteToggleForm component for improved performance and functionality

--- components/card/FavoriteToggleForm.tsx
+++ components/card/FavoriteToggleForm.tsx
@@ -5,8 +5,27 @@ import { FormContainer } from "../form/FormContainer";
 import { toggleFavoriteAction } from "@/utils/actions";
 import { CardSubmitButton } from "../form/Buttons";
 
-const FavoriteToggleForm = () => {
-  return <div>FavoriteToggleForm</div>;
+type FavoriteToggleFormProps = {
+  propertyId: string;
+  favoriteId: string | null;
+};
+
+const FavoriteToggleForm = ({
+  propertyId,
+  favoriteId,
+}: FavoriteToggleFormProps) => {
+  const pathname = usePathname();
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    propertyId,
+    favoriteId,
+    pathname,
+  });
+
+  return (
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
+  );
 };
 
 export default FavoriteToggleForm;
