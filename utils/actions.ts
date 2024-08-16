c68e4459c340f8b927ecf5b2f4a6d3cac9e803fd
Goran SD <g.cvetic23@gmail.com>
2024-08-16 16:09:09 -0700

    feat: Add fetchFavorites function for retrieving user's favorite properties

--- utils/actions.ts
+++ utils/actions.ts
@@ -249,3 +249,25 @@ export const toggleFavoriteAction = async (prevState: {
     return renderError(error);
   }
 };
+
+export const fetchFavorites = async () => {
+  const user = await getAuthUser();
+  const favorites = await db.favorite.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          tagline: true,
+          country: true,
+          price: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return favorites.map((favorite) => favorite.property);
+};
