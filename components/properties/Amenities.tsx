4193474c0807a24815a30bc7c85a41f3d594c746
Goran SD <g.cvetic23@gmail.com>
2024-08-17 14:35:34 -0700

    feat: Add Amenities component
    The commit adds the Amenities component to the `components/properties/Amenities.tsx` file. This component is used to display the amenities of a property. It currently returns a placeholder text "Amenities". This addition enhances the presentation of property amenities and improves the overall user experience of the application.

---
+++ components/properties/Amenities.tsx
@@ -0,0 +1,9 @@
+import { Amenity } from "@/utils/amenities";
+import { LuFolderCheck } from "react-icons/lu";
+import Title from "./Title";
+
+const Amenities = () => {
+  return <div>Amenities</div>;
+};
+
+export default Amenities;
