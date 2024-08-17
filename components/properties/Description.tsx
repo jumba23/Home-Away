1e316e7c3736b42b19ee39d4b0a69cd7d036a7f5
Goran SD <g.cvetic23@gmail.com>
2024-08-17 14:18:40 -0700

    feat: Refactor Description component to include show more/less functionality
    Refactor the Description component in `components/properties/Description.tsx` to include the functionality to show more or less of the property description. This enhancement allows users to toggle between displaying the full description or a truncated version, improving the user experience of the application.

--- components/properties/Description.tsx
+++ components/properties/Description.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import Title from "./Title";
 const Description = ({ description }: { description: string }) => {
   const [isFullDescriptionShown, setIsFullDescriptionShown] = useState(false);
-
   const words = description.split(" ");
   const isLongDescription = words.length > 100;
 
@@ -12,21 +11,22 @@ const Description = ({ description }: { description: string }) => {
     setIsFullDescriptionShown(!isFullDescriptionShown);
   };
 
-  const displayDescription =
-    isFullDescriptionShown && !isFullDescriptionShown
-      ? words.splice(0, 100).join(" ") + "..."
+  const displayedDescription =
+    isLongDescription && !isFullDescriptionShown
+      ? words.slice(0, 100).join(" ") + "..."
       : description;
 
   return (
     <article className="mt-4">
       <Title text="Description" />
-      <p>
-        {displayDescription && (
-          <Button variant="link" className="pl-0" onClick={toggleDescription}>
-            {isFullDescriptionShown ? "Show less" : "Show more"}
-          </Button>
-        )}
+      <p className="text-muted-foreground font-light leading-loose">
+        {displayedDescription}
       </p>
+      {isLongDescription && (
+        <Button variant="link" className="pl-0" onClick={toggleDescription}>
+          {isFullDescriptionShown ? "Show less" : "Show more"}
+        </Button>
+      )}
     </article>
   );
 };
