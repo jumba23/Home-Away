bda93ffc62755deddbb3e94504f93010a4a5aba7
Goran SD <g.cvetic23@gmail.com>
2024-08-17 13:57:08 -0700

    feat: Add lazy loading for images and Superhost information to UserInfo component
    Refactor the UserInfo component in `components/properties/UserInfo.tsx` to include lazy loading for the profile image using the next/image package. This improves the page load time, especially for users with large profile images. Additionally, add the Superhost information to the component, displaying the number of years the user has been hosting. This enhancement provides users with more information about the host and improves the overall user experience of the application.

--- components/properties/UserInfo.tsx
+++ components/properties/UserInfo.tsx
@@ -6,8 +6,24 @@ type UserInfoProps = {
     firstName: string;
   };
 };
-const UserInfo = () => {
-  return <div>UserInfo</div>;
+const UserInfo = ({ profile: { profileImage, firstName } }: UserInfoProps) => {
+  return (
+    <article className="grid grid-cols-[auto,1fr] gap-4 mt-4">
+      <Image
+        src={profileImage}
+        alt={firstName}
+        width={50}
+        height={50}
+        className="rounded w-12 h-12 object-cover"
+      />
+      <div>
+        <p>
+          Hosted by <span className="font-bold">{firstName}</span>
+        </p>
+        <p>Superhost &middot; 2 years hostings</p>
+      </div>
+    </article>
+  );
 };
 
 export default UserInfo;
