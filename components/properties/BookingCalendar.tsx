53cfc633913223f4ad7eea776a68fc8a90f7251f
Goran SD <g.cvetic23@gmail.com>
2024-08-17 07:47:54 -0700

    feat: Add BookingCalendar component to property details page
    The commit adds the BookingCalendar component to the property details page in the `components/properties/BookingCalendar.tsx` file. This component is imported and rendered in the `return` statement of the `PropertyDetailsPage` component. It displays a booking calendar for users to select dates for booking the property. This addition enhances the functionality of the application by providing an easy way for users to view and select available booking dates.
    
    Co-authored-by: [Author Name]

---
+++ components/properties/BookingCalendar.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { DateRange } from "react-day-picker";
+const BookingCalendar = () => {
+  return <div>BookingCalendar</div>;
+};
+
+export default BookingCalendar;
