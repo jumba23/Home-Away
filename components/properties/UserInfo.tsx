94375487050076d62cf5bfd6a6e8d16cc6bef244
Goran SD <g.cvetic23@gmail.com>
2024-08-17 13:50:37 -0700

    feat: Add UserInfo component
    The commit adds the UserInfo component to the `components/properties/UserInfo.tsx` file. This component is currently empty, but it provides a placeholder for future development and organization of the user information section. This addition lays the foundation for further enhancements and improvements to the user information functionality.

---
+++ components/properties/UserInfo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+type UserInfoProps = {
+  profile: {
+    profileImage: string;
+    firstName: string;
+  };
+};
+const UserInfo = () => {
+  return <div>UserInfo</div>;
+};
+
+export default UserInfo;
