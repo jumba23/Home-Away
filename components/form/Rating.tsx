993964844df172f82bfc049015076f2d4e926e97
Goran SD <g.cvetic23@gmail.com>
2024-08-18 16:31:50 -0700

    feat: Add RatingInput component
    Add RatingInput component to provide a UI element for rating properties. This enhancement improves the functionality and user experience of the application.

---
+++ components/form/Rating.tsx
@@ -0,0 +1,37 @@
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const RatingInput = ({
+  name,
+  labelText,
+}: {
+  name: string;
+  labelText?: string;
+}) => {
+  // create and array of 5 numbers from 1 to 5 in reverse order for the rating
+  const numbers = Array.from({ length: 5 }, (_, i) => {
+    const value = i + 1;
+    return value.toString();
+  }).reverse();
+
+  return (
+    <div className="mb-2 max-w-xs">
+      <Label htmlFor={name} className="capitalize">
+        {labelText || name}
+      </Label>
+      <Select defaultValue={numbers[0]} name={name} required>
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+      </Select>
+    </div>
+  );
+};
+
+export default RatingInput;
