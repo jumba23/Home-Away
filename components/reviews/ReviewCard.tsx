9ca75c2fe3a553300074a093ae1fffc70618f567
Goran SD <g.cvetic23@gmail.com>
2024-08-18 17:23:48 -0700

    feat: Add ReviewCard component for rendering individual review cards
    This commit adds the ReviewCard component to the project. The ReviewCard component is responsible for rendering individual review cards, including the comment, rating, name, and image of the reviewer. This enhancement improves the functionality and user experience of the application by providing a visually appealing and informative way to display property reviews.

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -1,3 +1,16 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Rating from "./Rating";
+import Comment from "./Comment";
+type ReviewCardProps = {
+  reviewInfo: {
+    comment: string;
+    rating: number;
+    name: string;
+    image: string;
+  };
+  children?: React.ReactNode;
+};
+
 const ReviewCard = () => {
   return <div>ReviewCard</div>;
 };
