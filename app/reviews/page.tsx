4c8364d0c077d426fdb60c30a6b8620def0190d1
Goran SD <g.cvetic23@gmail.com>
2024-08-18 23:34:31 -0700

    feat: Add EmptyList component and update imports in ReviewsPage
    Add the EmptyList component to the ReviewsPage file and update the imports accordingly. This enhancement improves the functionality of the ReviewsPage by providing a visually appealing way to display an empty list of reviews. It also ensures that the necessary components are imported correctly, enhancing the overall code organization and maintainability.

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,3 +1,13 @@
+import EmptyList from "@/components/home/EmptyList";
+import {
+  deleteReviewAction,
+  fetchPropertyReviewsByUser,
+} from "@/utils/actions";
+import ReviewCard from "@/components/reviews/ReviewCard";
+import Title from "@/components/properties/Title";
+import { FormContainer } from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+
 const ReviewsPage = () => {
   return <div>ReviewsPage</div>;
 };
