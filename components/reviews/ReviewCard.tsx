3ccd7ce96db859e3cb2d1b24ae112959a6ad74c3
Goran SD <g.cvetic23@gmail.com>
2024-08-18 17:28:50 -0700

    feat: Enhance ReviewCard component to include reviewer's image and name
    Improve the ReviewCard component by adding the reviewer's image and name. This enhancement enhances the functionality and user experience of the application by providing a more informative and visually appealing way to display property reviews.

--- components/reviews/ReviewCard.tsx
+++ components/reviews/ReviewCard.tsx
@@ -11,8 +11,30 @@ type ReviewCardProps = {
   children?: React.ReactNode;
 };
 
-const ReviewCard = () => {
-  return <div>ReviewCard</div>;
-};
-
+function ReviewCard({ reviewInfo, children }: ReviewCardProps) {
+  return (
+    <Card className="relative">
+      <CardHeader>
+        <div className="flex items-center">
+          <img
+            src={reviewInfo.image}
+            alt="profile"
+            className="w-12 h-12 rounded-full object-cover"
+          />
+          <div className="ml-4">
+            <h3 className="text-sm font-bold capitalize mb-1">
+              {reviewInfo.name}
+            </h3>
+            {/* <Rating rating={reviewInfo.rating} /> */}
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Comment comment={reviewInfo.comment} />
+      </CardContent>
+      {/* delete button later */}
+      <div className="absolute top-3 right-3">{children}</div>
+    </Card>
+  );
+}
 export default ReviewCard;
