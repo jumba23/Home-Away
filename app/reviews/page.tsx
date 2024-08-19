7de6dff1ab65f4990ddca1c9b8daa01c01117ba7
Goran SD <g.cvetic23@gmail.com>
2024-08-18 23:37:52 -0700

    feat: Enhance ReviewsPage with property review display and delete functionality
    Improve the ReviewsPage component by fetching and displaying property reviews by the authenticated user. This enhancement provides users with a personalized view of their own reviews, enhancing the functionality and user experience of the application. Additionally, add the ability to delete reviews using the DeleteReview component. This commit enhances the ReviewsPage component by adding valuable features and improving overall user satisfaction.

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -8,8 +8,41 @@ import Title from "@/components/properties/Title";
 import { FormContainer } from "@/components/form/FormContainer";
 import { IconButton } from "@/components/form/Buttons";
 
-const ReviewsPage = () => {
-  return <div>ReviewsPage</div>;
+const ReviewsPage = async () => {
+  const reviews = await fetchPropertyReviewsByUser();
+  if (reviews.length === 0) return <EmptyList />;
+
+  return (
+    <>
+      <Title text="Your Reviews" />
+      <section className="grid md:grid-cols-2 gap-8 mt-4 ">
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { name, image } = review.property;
+          const reviewInfo = {
+            comment,
+            rating,
+            name,
+            image,
+          };
+          return (
+            <ReviewCard key={review.id} reviewInfo={reviewInfo}>
+              <DeleteReview reviewId={review.id} />
+            </ReviewCard>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+const DeleteReview = ({ reviewId }: { reviewId: string }) => {
+  const deleteReview = deleteReviewAction.bind(null, { reviewId });
+  return (
+    <FormContainer action={deleteReview}>
+      <IconButton actionType="delete" />
+    </FormContainer>
+  );
 };
 
 export default ReviewsPage;
