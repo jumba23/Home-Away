65f1aafc77f16f2c5b4df031bcdef8ba87c146ed
Goran SD <g.cvetic23@gmail.com>
2024-08-20 14:54:46 -0700

    ```text feat: Update RentalsPage to display rentals in a table
    Refactor RentalsPage to display rentals in a table format. This enhancement improves the readability and organization of rental information, allowing users to easily view and interact with their rentals.

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -1,5 +1,56 @@
-const RentalsPage = () => {
-  return <div>RentalsPage</div>;
+import EmptyList from "@/components/home/EmptyList";
+import { fetchRentals, deleteRentalAction } from "@/utils/actions";
+import Link from "next/link";
+
+import { formatCurrency } from "@/utils/format";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { FormContainer } from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+
+const RentalsPage = async () => {
+  const rentals = await fetchRentals();
+  if (rentals.length === 0)
+    return (
+      <EmptyList
+        heading="No rentals to display"
+        message="Don't hesitate to create a rental"
+      />
+    );
+
+  return (
+    <div className="mt-16">
+      <h4 className="mb-4 capitalize">active properties: {rentals.length}</h4>
+      <Table>
+        <TableCaption>A list of your active rentals</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Property Name</TableHead>
+            <TableHead>Nightly Rate</TableHead>
+            <TableHead>Nights Booked</TableHead>
+            <TableHead>Total Income</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rentals.map((rental) => {
+            const { id: propertyId, name, price } = rental;
+            const { totalNightsSum, orderTotalSum } = rental;
+
+            return <TableRow key={propertyId}></TableRow>;
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default RentalsPage;
