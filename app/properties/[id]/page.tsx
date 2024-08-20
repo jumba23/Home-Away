eab34d86891f3031c5134313ba0de480f7cecfc8
Goran SD <g.cvetic23@gmail.com>
2024-08-19 22:41:52 -0700

    feat: Implement lazy loading for images in PropertyDetailsPage and Comment component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -41,7 +41,7 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
   const reviewDoesNotExist =
     userId && isNotOwner && !(await findExistingReview(userId, property.id)); // check if the review does not exist
 
-  console.log(property.bookings);
+  console.log("bookings -", property.bookings);
 
   return (
     <section>
