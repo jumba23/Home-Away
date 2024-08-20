8e238af4ca730d3fcd3c982422aa2f79a8ed4589
Goran SD <g.cvetic23@gmail.com>
2024-08-19 22:43:03 -0700

    feat: Remove BookingCalendar component and implement lazy loading for images in PropertyDetailsPage and Comment component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,5 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
-import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
 import PropertyDetails from "@/components/properties/PropertyDetails";
@@ -70,7 +69,6 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           {/*calendar*/}
-          <BookingCalendar />
         </div>
       </section>
       {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
