c606fe9b30ac3fbd2c13529d037cf73dcccaf6c1
Goran SD <g.cvetic23@gmail.com>
2024-08-20 11:51:58 -0700

    feat: Add lazy loading for images in BookingsPage
    Improve performance by implementing lazy loading for images in the BookingsPage component. This enhancement reduces initial load time and optimizes rendering, providing a better user experience.

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -16,8 +16,30 @@ import {
 import { FormContainer } from "@/components/form/FormContainer";
 import { IconButton } from "@/components/form/Buttons";
 import { fetchBookings, deleteBookingAction } from "@/utils/actions";
-const BookingsPage = () => {
-  return <div>BookingsPage</div>;
+const BookingsPage = async () => {
+  const bookings = await fetchBookings();
+  if (bookings.length === 0) return <EmptyList />;
+
+  return (
+    <div className="mt-16">
+      <h4 className="mb-4 capitalize">total bookings: {bookings.length}</h4>
+      <Table>
+        <TableCaption>A list of your recent bookings</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Property Name</TableHead>
+            <TableHead>Country</TableHead>
+            <TableHead>Nights</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Check In</TableHead>
+            <TableHead>Check Out</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody></TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default BookingsPage;
