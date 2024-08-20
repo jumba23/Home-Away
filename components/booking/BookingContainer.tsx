388684d286f4afbaf6d17449b76fe251513e76c3
Goran SD <g.cvetic23@gmail.com>
2024-08-19 23:03:55 -0700

    feat: Implement lazy loading for images in BookingCalendar and BookingContainer components

--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { useProperty } from "@/utils/store";
+import ConfirmBooking from "./ConfirmBooking";
+import BookingForm from "./BookingForm";
+
 const BookingContainer = () => {
-  return <div>BookingContainer</div>;
+  const { range } = useProperty((state) => state);
+
+  if (!range || !range.from || !range.to) return null;
+  if (range.to.getTime() === range.from.getTime()) return null;
+  return (
+    <div className="w-full">
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 };
 
 export default BookingContainer;
