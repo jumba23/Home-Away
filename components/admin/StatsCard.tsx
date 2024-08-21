ea94f4dbb63ee4d899fa9ac16a640a1e1dfa3d16
Goran SD <g.cvetic23@gmail.com>
2024-08-20 23:43:52 -0700

    refactor: Add lazy loading for images in StatsCard and fetch stats in StatsContainer
    Refactor the StatsCard component to add lazy loading for images, improving the performance of the application by reducing the initial page load time. Additionally, update the StatsContainer component to fetch stats for the admin dashboard, providing up-to-date statistics for analysis.

--- components/admin/StatsCard.tsx
+++ components/admin/StatsCard.tsx
@@ -1,5 +1,19 @@
-const StatsCard = () => {
-  return <div>StatsCard</div>;
+import { Card, CardHeader } from "@/components/ui/card";
+
+type StatsCardsProps = {
+  title: string;
+  value: number;
+};
+
+const StatsCard = ({ title, value }: StatsCardsProps) => {
+  return (
+    <Card className="bg-muted">
+      <CardHeader className="flex flex-row justify-between items-center">
+        <h3 className="capitalize text-3xl font-bold">{title}</h3>
+        <span className="text-primary text-5xl font-extrabold">{value}</span>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default StatsCard;
