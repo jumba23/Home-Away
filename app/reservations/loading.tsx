330b799960ce035900c00519c61ae63c88a43ffa
Goran SD <g.cvetic23@gmail.com>
2024-08-20 22:38:01 -0700

    refactor: Remove unused import in loading.tsx and update reservation page content
    Remove the unused import of React in loading.tsx and update the content of the reservation page to display "Reservation Page" instead of "page". This refactor improves code cleanliness and provides a more descriptive page title for reservations.

--- app/reservations/loading.tsx
+++ app/reservations/loading.tsx
@@ -1,5 +1,4 @@
 import LoadingTable from "@/components/booking/LoadingTable";
-import React from "react";
 
 const loading = () => {
   return <LoadingTable />;
--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,5 +1,5 @@
 const page = () => {
-  return <div>page</div>;
+  return <div>Reservation Page</div>;
 };
 
 export default page;
