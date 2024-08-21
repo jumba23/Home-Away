f15e1ce97efe91bd0b656a3d830fbf00e21ad2d2
Goran SD <g.cvetic23@gmail.com>
2024-08-20 23:38:36 -0700

    feat: Add lazy loading for images and stats in admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
+import ChartsContainer from "@/components/admin/ChartsContainer";
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from "@/components/admin/Loading";
+import StatsContainer from "@/components/admin/StatsContainer";
+import { Suspense } from "react";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 };
 
 export default AdminPage;
