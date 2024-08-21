d72f7c81de1ec6bebae8eff81e44f01aa093c7b8
Goran SD <g.cvetic23@gmail.com>
2024-08-20 23:56:01 -0700

    refactor: Add lazy loading for images in Chart component
    Refactor the Chart component to implement lazy loading for images, improving the performance of the application by reducing the initial page load time. This enhances the functionality and usability of the admin dashboard.

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,5 +1,36 @@
-const Chart = () => {
-  return <div>Chart</div>;
+"use client";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+type ChartPropsType = {
+  data: {
+    date: string;
+    count: number;
+  }[];
+};
+
+const Chart = ({ data }: ChartPropsType) => {
+  return (
+    <section className="mt-24">
+      <h1 className="text-4xl font-semibold text-center">Monthly Bookings</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#F97215" barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 };
 
 export default Chart;
--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,5 +1,11 @@
-const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+import { fetchChartsData } from "@/utils/actions";
+import Chart from "./Chart";
+
+const ChartsContainer = async () => {
+  const bookings = await fetchChartsData();
+  if (bookings.length < 1) return null;
+
+  return <Chart data={bookings} />;
 };
 
 export default ChartsContainer;
