0a313b9f4859a0632fbbd75e2e40f061a92e1447
Goran SD <g.cvetic23@gmail.com>
2024-08-20 23:24:17 -0700

    refactor: Update middleware to protect admin routes and add public route exemption
    The middleware.ts file has been refactored to update the logic for protecting admin routes. The code now checks if the user is an admin user and redirects to the homepage if they are not authorized to access the admin routes. Additionally, a new isPublicRoute function has been added to exempt certain routes from authentication. This refactor improves the security and accessibility of the application.

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/bookings(.*)",
-  "/checkout(.*)",
-  "/favorites(.*)",
-  "/profile(.*)",
-  "/rentals(.*)",
-  "/reviews(.*)",
-]);
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/bookings(.*)",
+//   "/checkout(.*)",
+//   "/favorites(.*)",
+//   "/profile(.*)",
+//   "/rentals(.*)",
+//   "/reviews(.*)",
+// ]);
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]); // define the route that is only accessible by the admin user
+export default clerkMiddleware(async (auth, req) => {
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID; // check if the user is an admin user
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (!isPublicRoute(req)) auth().protect();
+}); // protect the route that is not public
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"], //
 };
