e1e3a27f9f9747af1a1ffdbbf00f9a7772196319
Goran SD <g.cvetic23@gmail.com>
2024-08-22 22:55:48 -0700

    refactor: Add lazy loading for images in CheckoutPage component
    Refactor the CheckoutPage component to add lazy loading for images, improving the performance of the application by reducing the initial page load time. This enhances the functionality and usability of the checkout process.

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+
+// this is where we establish the connection to the Stripe API
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
 const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  const searchParams = useSearchParams();
+  const bookingId = searchParams.get("bookingId");
+
+  // call back to our server to get the client secret
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      bookingId,
+    });
+    return response.data.clientSecret;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const options = {
+    fetchClientSecret,
+  };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
 };
 
 export default CheckoutPage;
